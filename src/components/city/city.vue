731877567576fcedc04aff3e71a5d68ad22bd795
violet-lu <violetlu94@gmail.com>
2021-12-13 21:50:51 +0800

    'city'

--- src/components/city/city.vue
+++ src/components/city/city.vue
@@ -2,17 +2,20 @@
   <div class="city">
     <city-header></city-header>
     <city-recent></city-recent>
+    <city-alphbet></city-alphbet>
   </div>
 </template>
 
 <script>
 import CityHeader from './component/cityheader.vue'
 import CityRecent from './component/cityrecent.vue'
+import CityAlphbet from './component/cityalphbet.vue'
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CityRecent
+    CityRecent,
+    CityAlphbet
   }
 }
 </script>
